eb5acae1179d4e3064f265e05f5ef6fabdb0f544
Daniel Tierney <daniel.tierney1@icloud.com>
2025-04-13 22:39:33 +0100

    Update QRGenerator.jsx
    Commented out old redundant code. Potential full deletion at a later date

--- Qr_Web_App/src/Components/QRGenerator.jsx
+++ Qr_Web_App/src/Components/QRGenerator.jsx
@@ -66,48 +66,48 @@ const QRGenerator = () => {
         }
     }, [ input, dotStyle, dotColor, eyeStyle, bgColor, image, errorCorrection ]);
 
-    return (
-        <div style={{padding: '1rem'}}>
-            <h2>QR Generator</h2>
+    // return (
+    //     <div style={{padding: '1rem'}}>
+    //         <h2>QR Generator</h2>
 
-            {/* User input for QR Generator Component */}
-            <input
-            type="text"
-            placeholder="Enter text or URL"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            style={{ width: '100%', padding: '0.5rem', fontSize: '1rem'}}
-            />
+    //         {/* User input for QR Generator Component */}
+    //         <input
+    //         type="text"
+    //         placeholder="Enter text or URL"
+    //         value={input}
+    //         onChange={(e) => setInput(e.target.value)}
+    //         style={{ width: '100%', padding: '0.5rem', fontSize: '1rem'}}
+    //         />
 
-            {/* Dropdown menu for error correction */}
-            <div style={{margin: '1rem 0'}}>
-                <label>
-                    Error Correction Label:&nbsp;
-                    <select
-                    value={errorCorrection}
-                    onChange={(e) => setErrorCorrection(e.target.value)}
-                    >
-                        <option value="L">L (Low)       </option>
-                        <option value="M">M (Medium)    </option>
-                        <option value="Q">Q (Quartile)  </option>
-                        <option value="H">H (High)      </option>
-                    </select>
-                </label>
-            </div>
+    //         {/* Dropdown menu for error correction */}
+    //         <div style={{margin: '1rem 0'}}>
+    //             <label>
+    //                 Error Correction Label:&nbsp;
+    //                 <select
+    //                 value={errorCorrection}
+    //                 onChange={(e) => setErrorCorrection(e.target.value)}
+    //                 >
+    //                     <option value="L">L (Low)       </option>
+    //                     <option value="M">M (Medium)    </option>
+    //                     <option value="Q">Q (Quartile)  </option>
+    //                     <option value="H">H (High)      </option>
+    //                 </select>
+    //             </label>
+    //         </div>
 
-            {/* QR Code SVG Rendering */}
-            { input && (
-                <div style={{margin:"1rem 0"}}>
-                    <QRCodeSVG
-                    value={input}
-                    size={256}
-                    level={errorCorrection}
-                    includeMargin={true}
-                    />
-                </div>
-            )}
-        </div>
-    )
+    //         {/* QR Code SVG Rendering */}
+    //         { input && (
+    //             <div style={{margin:"1rem 0"}}>
+    //                 <QRCodeSVG
+    //                 value={input}
+    //                 size={256}
+    //                 level={errorCorrection}
+    //                 includeMargin={true}
+    //                 />
+    //             </div>
+    //         )}
+    //     </div>
+    // )
 }
 
 export default QRGenerator;
