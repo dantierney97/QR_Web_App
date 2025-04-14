b1811ed9f32bc46da256ed75c4b38d081c306029
Daniel Tierney <daniel.tierney1@icloud.com>
2025-04-14 20:37:48 +0100

    Updated vite.config.js
    Added Vite-Plugin-PWA to the config with generic settings added

--- Qr_Web_App/vite.config.js
+++ Qr_Web_App/vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg', 'assets/icon-192.png', 'assets/icon-512.png'],
+            manifest: {
+                name: 'QR Code Generator',
+                short_name: 'QRGen',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#1e1e1e',
+                icons: [
+                    {
+                        src: 'assets/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'assets/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
+});
